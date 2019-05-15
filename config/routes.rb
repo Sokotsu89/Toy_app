7de6a3e4e6477cb0da322253a37451a00d81dd86
Christopher Caba <ccaba89@gmail.com>
2019-05-15 14:24:45 +0000

    Made more chenges to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
-root 'application#hello'
+  resources :microposts
+  resources :users
+  root 'users#index'
 end
